bb3c7857305d89cda029a49fcc5e273eb086c633
Personalstorage <personalstoragevenky@gmail.com>
2026-04-24 06:39:52 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3449247481-c47c7",
+  "appId": "1:807462069336:web:e36936ae0e440be9f2562d",
+  "apiKey": "AIzaSyDpKb5jMayinSquVOBpNC79KI6O33sAv9Q",
+  "authDomain": "studio-3449247481-c47c7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "807462069336"
+};
